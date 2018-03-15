8d0bba591b63c2a6582daad7fa9eabf540a22fd2
pedroken3 <33911676+pedroken3@users.noreply.github.com>
2018-03-15 16:19:42 +0100

    While7 terminado
    este me ha dado mas problemas que todos los demas juntos, el cero me lo da como par y no he podido evitarlo y he aprendido que se puede poner if o else sin{}

---
+++ ejercicios/src/ejercicios/While7.java
@@ -0,0 +1,40 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pedroken
+ */
+public class While7 {
+    public static void main (String[] args){
+        
+        int i=1,num,valor=0,impar=0,par=0,nd=0;
+        
+        Scanner operador=new Scanner(System.in);
+        
+        System.out.print("Introduce candidad de números que vas a comprobar: ");
+        num=operador.nextInt();
+        
+        while(i<=num){
+            System.out.print("Introduce los números: ");
+            valor=operador.nextInt();
+            
+        if(valor%2==0)
+            par=par+1;            
+
+        else
+            impar=impar+1;        
+            i++;
+        }
+            System.out.println("son par: "+par);
+            System.out.println("son impar: "+impar);
+
+    }    
+}
+/**Desarrollar un programa que permita cargar n números enteros y luego nos
+  informe cuántos valores fueron pares y cuántos impares.
+Emplear el operador ?%? en la condición de la estructura condicional:
+
+	if (valor%2==0)         //Si el if da verdadero luego es par.
+**/
