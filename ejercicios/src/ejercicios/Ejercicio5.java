25dc8af649e60b8d60388da24c5dc34f9b01078a
pedroken3 <33911676+pedroken3@users.noreply.github.com>
2018-03-19 20:59:48 +0100

    perfeccionado el 5 terminado el 6
    para que no se me olvide, lo que no tengo claro es como guardar los numeros por separado y como hacer para no tener que aclararle al programa la cantidad de numeros a introducir,osea puedas pedir cada vez una cantidad distinta

--- ejercicios/src/ejercicios/Ejercicio5.java
+++ ejercicios/src/ejercicios/Ejercicio5.java
@@ -14,14 +14,15 @@ import java.util.Scanner;
 public class Ejercicio5 {
     public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
-        int num1,suma=0,i=1;
-        double promedio=0;
+        int num1,i=1,s=1;
+        double promedio=0,suma=0;
         while(i<=4){
-        System.out.println("Introduce los valores: ");
+        System.out.println("Introduce el número "+s+":");
         num1=teclado.nextInt();
         suma+=num1;
         promedio=suma/4;
         i++;
+        s++;
         }
         System.out.println("El resultado de la suma es: "+suma);
         System.out.println("El resultado promedio es: "+promedio);
