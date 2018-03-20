c49ffb3565597f61dd3a6fd9a823fd17c2718f29
pedroken3 <33911676+pedroken3@users.noreply.github.com>
2018-03-20 20:51:57 +0100

    no tengo muy claro else
    no se como me lo monto pero si pongo{} me da error y si no lo pongo me va bien.

---
+++ ejercicios/src/ejercicios/Ejercicio14.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pedroken
+ */
+public class Ejercicio14 {
+    public static void main(String[]ar){
+    Scanner teclado=new Scanner(System.in);
+    int num1,num2,num3;
+    
+    System.out.print("Introduce el primer número: ");
+    num1=teclado.nextInt();
+    
+    System.out.print("Introduce el segundo número: ");
+    num2=teclado.nextInt();
+    
+    System.out.print("Introduce el tercer número: ");
+    num3=teclado.nextInt();
+    
+    if(num1>num2&&num1>num3)
+        System.out.println("El primer número es el mayor: "+num1);
+    else if (num2>num1&&num2>num3)
+        System.out.println("El segundo número es el mayor: "+num2);
+    //if(num3>num1&&num3>num2) 
+    else
+        System.out.println("El tercer número es el mayor: "+num3);
+                
+            
+    
+    }
+    
+}
+/**Se cargan por teclado tres números distintos. Mostrar por pantalla el mayor de ellos.**/
