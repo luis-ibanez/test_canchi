f5af36dbba9e01e91399b8fa145e1256bb2981e5
Luis Ibanez <luis@the3rocks.com>
2018-03-26 17:09:36 +0200

    New solution with arrays

--- ejercicios/src/ejercicios/Ejercicio25.java
+++ ejercicios/src/ejercicios/Ejercicio25.java
@@ -5,28 +5,54 @@ import java.util.Scanner;
 public class Ejercicio25 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int num1, num2, num3;
+        int[] numerosOrdenados = new int[3];
+
         System.out.print("Introduce el número 1: ");
-        num1 = teclado.nextInt();
+        numerosOrdenados[0] = teclado.nextInt();
         System.out.print("Introduce el número 2: ");
-        num2 = teclado.nextInt();
+        numerosOrdenados[1] = teclado.nextInt();
         System.out.print("Introduce el número 3: ");
-        num3 = teclado.nextInt();
+        numerosOrdenados[2] = teclado.nextInt();
 
-        if (num1 > num2 && num1 > num3) {
-            System.out.println("El número 1 es el mayor: " + num1);
+        //10 9 8 7 6 5 4 3 2 1
+        //9 8 7 6 5 4 3 2 1 10
+        //8 7 6 5 4 3 2 1 9 10
 
-        } else if (num2 > num3) {
-            System.out.println("El número 2 es el mayor: " + num2);
-        } else {
-            System.out.println("El número 3 es el mayor: " + num3);
-        }if (num1 < num2 && num1 < num3){
-            System.out.println("El número 1 es el menor: "+num1);
-        }else if (num2 < num3){
-            System.out.println("El número 2 es el mayor: "+num2);
-        }else{
-            System.out.println("El número 3 es el mayor: "+num3);
+        //10x10 = 100
+        //100x100 = 10000
+        //coste cuadratico
+
+        //3 2 1
+        //if num[0] > num[1]
+        //num[0] = num[1], num[1] = num[0]
+        //2 3 1
+        //if num[1] > num[2]
+        //num[1] = num[2], num[2] = num[1]
+        //2 1 3
+        //if num[0] > num[1]
+        //num[0] = num[1], num[1] = num[0]
+        //1 2 3
+
+        if (numerosOrdenados[0] > numerosOrdenados[1]){
+            int temp = numerosOrdenados[0];
+            numerosOrdenados[0] = numerosOrdenados[1];
+            numerosOrdenados[1] = temp;
         }
+
+        if (numerosOrdenados[1] > numerosOrdenados[2]){
+            int temp = numerosOrdenados[1];
+            numerosOrdenados[1] = numerosOrdenados[2];
+            numerosOrdenados[2] = temp;
+
+            if(numerosOrdenados[0] > numerosOrdenados[1]){
+                temp = numerosOrdenados[0];
+                numerosOrdenados[0] = numerosOrdenados[1];
+                numerosOrdenados[1] = temp;
+            }
+        }
+
+        System.out.println("El número mayor es el: "+numerosOrdenados[2]);
+        System.out.println("El número menor es el: "+numerosOrdenados[0]);
     }
 }
 /*7.Escribir un programa en el cual: dada una lista de tres valores numéricos distintos
