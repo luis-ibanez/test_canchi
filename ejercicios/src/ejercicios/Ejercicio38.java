736daef47a50307aa8d44096ec1f1d8fbbeca0e8
pedroken3 <33911676+pedroken3@users.noreply.github.com>
2018-03-27 20:43:09 +0200

    37 me he calentaoo
    queria sacar las tablas de multiplicar por que queria yo y me ha costado pero conseguido me hacia unas cosas muy raras jajajaja aprender jugando mola

---
+++ ejercicios/src/ejercicios/Ejercicio38.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio38 {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        
+    }
+}
+/*Escribir un programa que solicite la carga de un número entre 0 y 999, y nos
+ muestre un mensaje de cuántos dígitos tiene el mismo. Finalizar el programa
+  cuando se cargue el valor 0.*/
