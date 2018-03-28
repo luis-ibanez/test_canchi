dadec89b0e107ee5a693f4b0d6240e0bf0865ec9
pedroken3 <33911676+pedroken3@users.noreply.github.com>
2018-03-28 10:08:07 +0200

    empiezo do while
    me esta gustando esto

---
+++ ejercicios/src/ejercicios/Ejercicio37.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+public class Ejercicio37 {
+    public static void main(String[] args){
+        int f, num = 1, total;
+        for (f = 1; f <= 10; f++){
+           // System.out.println(num + "*" + f + "=" + num * f);
+            for (num = 1; num <= 10; num++){
+
+                System.out.println(num + " X " + f + " = " + num * f);
+            }
+
+        }
+    }
+}
--- ejercicios/src/ejercicios/Ejercicio38.java
+++ ejercicios/src/ejercicios/Ejercicio38.java
@@ -3,9 +3,26 @@ package ejercicios;
 import java.util.Scanner;
 
 public class Ejercicio38 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        
+        int num;
+
+        do {
+            System.out.print("Escribe un número del 0 al 999(escribe 0 para salir): ");
+            num = teclado.nextInt();
+
+            if (num > 0 && num < 10) {
+                System.out.println("Tu número tiene un digito.");
+            } else if (num > 9 && num < 100) {
+                System.out.println("Tu número tiene dos digitos.");
+            } else if (num > 99 && num < 1000) {
+                System.out.println("Tu número tiene tres digitos.");
+            } else if (num >= 1000 || num <= -1){
+                System.out.println("Tont@ de los cojones lee bien.");
+            }
+        }
+        while (num != 0);
+        System.out.println("Gracias por tus 5 duros.");
     }
 }
 /*Escribir un programa que solicite la carga de un número entre 0 y 999, y nos
