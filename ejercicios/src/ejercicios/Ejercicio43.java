764f2d13c50bfbe36536ae9a671ba38fad918458
pedroken3 <33911676+pedroken3@users.noreply.github.com>
2018-03-30 21:04:26 +0200

    para flipar
    esto por que lo hace, si pongo los dos String juntos va bien si los separo pasa del segundo?????

---
+++ ejercicios/src/ejercicios/Ejercicio43.java
@@ -0,0 +1,33 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio43 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int edad1, edad2;
+        String nom1, nom2;
+
+        System.out.println("Introduce el nombre de la primera persona: ");
+        nom1 = teclado.nextLine();
+        //si quitas de aqui la segunda persona y la pones abajo no hace la pregunta?????
+        System.out.println("Introduce el nombre de la segunda persona: ");
+        nom2 = teclado.nextLine();
+        System.out.println("Introduce la edad de la primera persona: ");
+        edad1 = teclado.nextInt();
+        //System.out.println("Introduce el nombre de la segunda persona: ");
+        //nom2 = teclado.nextLine();
+
+
+        System.out.println("Introduce la edad de la segunda persona: ");
+        edad2 = teclado.nextInt();
+
+        if (edad1 > edad2){
+            System.out.println(nom1 + " es la persona de mas edad: ");
+        }else {
+            System.out.println(nom2 + " es la persona de mas edad: ");
+        }
+    }
+}
+/*Solicitar el ingreso del nombre y edad de dos personas. Mostrar el nombre de
+ la persona con mayor edad.*/
