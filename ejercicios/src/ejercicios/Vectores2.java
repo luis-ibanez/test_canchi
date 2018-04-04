8cc772f13c67ac86a291075ed9fed98a6f1ac1ba
pedroken3 <33911676+pedroken3@users.noreply.github.com>
2018-04-04 10:10:19 +0200

    Al final aprenderere
    le voy cogiendo el hilo a esta mierda jajajajaja

---
+++ ejercicios/src/ejercicios/Vectores2.java
@@ -0,0 +1,47 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Vectores2 {
+    private Scanner teclado;
+    private int[] elementos = new int[8];
+    private int may1, may2, suma, i = 1;
+
+    public void carga() {
+        Scanner teclado = new Scanner(System.in);
+        for (int f = 0; f < 8; f++) {
+            System.out.print("Intruce el valor " + i + ": ");
+            elementos[f] = teclado.nextInt();
+            suma += elementos[f];
+            i++;
+        }
+    }
+
+    public void comparacion() {
+        for (int f = 0; f < 8; f++) {
+            if (elementos[f] > 36) {
+                may1 += elementos[f];
+            }
+            if (elementos[f] > 50) {
+                may2 += elementos[f];
+            }
+        }
+    }
+
+    public void imprimir() {
+        System.out.println("El valor acumulado total es de: " + suma);
+        System.out.println("El valor acumulado de los elementos mayores a 36 es: " + may1);
+        System.out.println("El valor acumulado de los elementos mayores a 50 es: " + may2);
+    }
+
+    public static void main(String[] args) {
+        Vectores2 elem = new Vectores2();
+        elem.carga();
+        elem.comparacion();
+        elem.imprimir();
+    }
+}
+/*Desarrollar un programa que permita ingresar un vector de 8 elementos, e informe:
+El valor acumulado de todos los elementos del vector.
+El valor acumulado de los elementos del vector que sean mayores a 36.
+Cantidad de valores mayores a 50.*/
