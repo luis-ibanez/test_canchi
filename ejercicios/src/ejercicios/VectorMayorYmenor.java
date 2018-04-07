f3f050a3fa485342f98a1d7449806caad13a28bb
pedroken3 <33911676+pedroken3@users.noreply.github.com>
2018-04-07 21:02:21 +0200

    Que estressss
    espero que no este mal guardarme todos los ejercicios para luegoadaptarlos a lo que necesite.
    Porque de todo esto no me acuerdo ni de coña

---
+++ ejercicios/src/ejercicios/VectorMayorYmenor.java
@@ -0,0 +1,48 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class VectorMayorYmenor {
+    private Scanner teclado = new Scanner(System.in);
+    private String[] nombre = new String[5];
+    private int[] sueldo = new int[5];
+
+    public void carga() {
+        for (int f = 0; f < sueldo.length; f++) {
+            System.out.print("Introduce el nombre del operario: ");
+            nombre[f] = teclado.next();
+            System.out.print("Introduce el sueldo del operario: ");
+            sueldo[f] = teclado.nextInt();
+        }
+    }
+
+    public void comparar() {
+        if (sueldo[0] > sueldo[1] && sueldo[0] > sueldo[2] && sueldo[0] >
+                sueldo[3] && sueldo[0] > sueldo[4]) {
+            System.out.print("El sueldo mayor es el de: " + nombre[0]);
+        }
+        if (sueldo[1] > sueldo[0] && sueldo[1] > sueldo[2] && sueldo[1] >
+                sueldo[3] && sueldo[1] > sueldo[4]) {
+            System.out.print("El sueldo mayor es el de: " + nombre[1]);
+        }
+        if (sueldo[2] > sueldo[1] && sueldo[2] > sueldo[0] && sueldo[2] >
+                sueldo[3] && sueldo[2] > sueldo[4]) {
+            System.out.print("El sueldo mayor es el de: " + nombre[2]);
+        }
+        if (sueldo[3] > sueldo[1] && sueldo[3] > sueldo[2] && sueldo[3] >
+                sueldo[0] && sueldo[3] > sueldo[4]) {
+            System.out.print("El sueldo mayor es el de: " + nombre[3]);
+        }else{
+            System.out.print("El sueldo mayor es el de: " + nombre[4]);
+        }
+    }
+    public static void main(String[] args){
+        VectorMayorYmenor toma = new VectorMayorYmenor();
+        toma.carga();
+        toma.comparar();
+    }
+}
+/*Problema 1:
+
+Confeccionar un programa que permita cargar los nombres de 5 operarios y
+sus sueldos respectivos. Mostrar el sueldo mayor y el nombre del operario.*/
---
+++ ejercicios/src/ejercicios/VectoresOrdenados2.java
@@ -0,0 +1,55 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class VectoresOrdenados2 {
+    private Scanner teclado = new Scanner(System.in);
+    private String[] paises = new String[5];
+
+    public void carga() {
+        for (int f = 0; f < 5; f++) {
+            System.out.print("Escribe el nombre del país: ");
+            paises[f] = teclado.next();
+        }
+    }
+
+    public void comparar() {
+        for (int i = 0; i < (paises.length - 1); i++) {
+            for (int f = i + 1; f < paises.length; f++) {
+                if (paises[i].compareToIgnoreCase(paises[f]) > 0) {
+                    String temp;
+                    temp = paises[i];
+                    paises[i] = paises[f];
+                    paises[f] = temp;
+                }
+            }
+
+        }
+    }
+
+    public void imprimir() {
+        for (int f = 0; f < 5; f++) {
+            System.out.println(paises[f]);
+        }
+    }
+    
+    public static void main(String[] args) {
+        VectoresOrdenados2 toma = new VectoresOrdenados2();
+        toma.carga();
+        toma.comparar();
+        toma.imprimir();
+    }
+}
+/*Problema 2:
+
+Definir un vector donde almacenar los nombres de 5 paises. Confeccionar
+ el algoritmo de ordenamiento alfabético.*/
+
+
+
+
+
+
+
+
+
