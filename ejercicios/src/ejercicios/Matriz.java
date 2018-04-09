93f70e60f6167e3eae655f8d3e3281373a4e7592
pedroken3 <33911676+pedroken3@users.noreply.github.com>
2018-04-09 13:43:23 +0200

    Matrices a mi bola
    la web de los ejercicios es una mierda para explicarte como se hacen las cosas +*@#%&#@$

---
+++ ejercicios/src/ejercicios/Matriz.java
@@ -0,0 +1,39 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Matriz {
+    private Scanner teclado = new Scanner(System.in);
+    private int[][] matriz = new int[3][5];
+    private int l, c;
+    public void carga(){
+
+        for (int f = 0; f < 3; f++){
+            System.out.println("Introduce los datos de la fila " + f + ": ");
+
+
+            for (c = 0; c < 5; c++){
+                System.out.print("Introduce los datos de la columna " + c + ": ");
+                matriz[f][c] = teclado.nextInt();
+            }
+        }
+    }
+    public void Imprimir(){
+        for (int f = 0; f < 3; f++){
+
+            for (int c = 0; c < 5; c++){
+                System.out.print(matriz[f][c] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Matriz toma = new Matriz();
+        toma.carga();
+        toma.Imprimir();
+    }
+}
+/*Problema 1:
+
+Crear una matriz de 3 filas por 5 columnas con elementos de tipo int, cargar
+ sus componentes y luego imprimirlas.*/
