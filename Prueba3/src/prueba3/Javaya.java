fb7b01bd6647f7273c423ae33c2e3a882a476e56
pedroken3 <33911676+pedroken3@users.noreply.github.com>
2018-09-11 11:58:00 +0200

    juego números
    creo que mi subconsciente quiere decirme algo jajjaajjajaa
    FOREST

--- Prueba3/src/prueba3/Javaya.java
+++ Prueba3/src/prueba3/Javaya.java
@@ -1,81 +1,43 @@
 package prueba3;
 
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Javaya {
+
     private Scanner teclado = new Scanner(System.in);
-    private String pais[] = new String[5];
-    private int poblacion[] = new int[5];
+    private Random numero = new Random();
+    private int numeroAleatorio = numero.nextInt(999);
+    private int jugador = -1;
+
+    public void DesarrolloJuego() {
+        while (jugador != numeroAleatorio) {
+            System.out.println("Introduce un número entre 0 y 999: ");
+            jugador = teclado.nextInt();
+            if (jugador < numeroAleatorio) {
+                System.out.println("Te has quedado cort@, intenta con otro número. ");
+            } else if (jugador > numeroAleatorio) {
+                System.out.println("Prueba un número mas pequeño.");
+            }
+        }
+        System.out.println("PREMIOOOOOOOOO");
+    }
 
     public void Imprimir() {
-        for (int f = 0; f < pais.length; f++) {
-            System.out.println(pais[f] + ": " + poblacion[f]);
-        }
-    }
-
-    public void CargarPaisPoblacion() {
-        for (int f = 0; f < pais.length; f++) {
-            System.out.println("Introduce el " + (f + 1) + "º " + "pais: ");
-            pais[f] = teclado.next();
-            System.out.println("Introduce la poblacion del " + (f + 1) + "º " + "pais: ");
-            poblacion[f] = teclado.nextInt();
-        }
-    }
-
-    public void CompararPaisOrdenAlfabetico() {
-        for (int c = 0; c < pais.length - 1; c++) {
-            for (int f = 0; f < pais.length -1; f++) {
-                String nombreTemporal;
-                int cantidadDeGenteTemporal = 0;
-                int x = (f +1);
-                if (pais[f].compareTo(pais[x]) > 0) {
-                    nombreTemporal = pais[f];
-                    pais[f] = pais[x];
-                    pais[x] = nombreTemporal;
-
-                    cantidadDeGenteTemporal = poblacion[f];
-                    poblacion[f] = poblacion[x];
-                    poblacion[x] = cantidadDeGenteTemporal;
-                }
-            }
-        }
-    }
-
-    public void CompararPaisPorPoblacion() {
-        for (int c = 0; c < pais.length - 1; c++) {
-            for (int f = 0; f < pais.length -1; f++) {
-                String nombreTemporal;
-                int poblacionTemporal = 0;
-                int x = (f + 1);
-                if (poblacion[f] < poblacion[x]) {
-                    poblacionTemporal = poblacion[f];
-                    poblacion[f] = poblacion[x];
-                    poblacion[x] = poblacionTemporal;
-
-                    nombreTemporal = pais[f];
-                    pais[f] = pais[x];
-                    pais[x] = nombreTemporal;
-                }
-            }
-        }
+        System.out.println(numeroAleatorio);
     }
 
     public static void main(String[] args) {
-        Javaya ordenPaisPoblacion = new Javaya();
-        ordenPaisPoblacion.CargarPaisPoblacion();
-        ordenPaisPoblacion.CompararPaisOrdenAlfabetico();
-        System.out.println("Orden alfabetico de los paises.");
-        ordenPaisPoblacion.Imprimir();
-        ordenPaisPoblacion.CompararPaisPorPoblacion();
-        System.out.println("Orden de los paises por poblacion.");
-        ordenPaisPoblacion.Imprimir();
+        Javaya juegoNumeros = new Javaya();
+        juegoNumeros.Imprimir();
+        juegoNumeros.DesarrolloJuego();
     }
 
 }
 
 
-/*Cargar en un vector los nombres de 5 paises y en otro vector paralelo la cantidad de habitantes del mismo.
- Ordenar alfabéticamente e imprimir los resultados. Por último ordenar con respecto a la cantidad de habitantes (de mayor a menor) e imprimir nuevamente.*/
+/*Juego
+* crear un juego que pida numeros hasta que aciertes el que genera al programa.*/
 
 //System.out.println();
